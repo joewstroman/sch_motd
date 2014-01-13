651cdbe56cc0bb00cc44c30066a705adc896de08
Joseph Stroman <Joseph@scholasticits-MacBook-Pro-2.local>
2014-01-13 09:51:49 -0500

    Changed syntax to be compatible with earlier ruby versions

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'me@chr4.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures motd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.1'
+version          '0.5.2'
 depends          'chef_handler'
 depends          'cron'
